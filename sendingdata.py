96451e647dbe5a266a4c00ce6abc7fc3742d4d5e
redhood-97 <anishksaha1997@gmail.com>
2019-03-22 18:26:54 +0530

    initial commit

--- sendingdata.py
+++ sendingdata.py
@@ -158,4 +158,4 @@ def watch():
                 return response
 
 if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=8080, debug=True)
+        app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
